d9c2aee67fd6be89fd10843e9d6c83e5353a2880
Bkorob <baka311@yandex.ru>
2024-04-08 21:54:19 +0300

    step4 создание модели

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view(), name='home'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
-    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('', views.IndexView.as_view(), name='home'),
+    path('login/', views.UserLoginFormView.as_view(), name='login'),
+    path('logout/', views.UserLogoutFormView.as_view(), name='logout'),
     path('users/', include('task_manager.users.urls')),
 #     path('statuses/', include('task_manager.statuses.urls')),
 #     path('tasks/', include('task_manager.tasks.urls')),
