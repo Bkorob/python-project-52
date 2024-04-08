7c80f16c8337e78ec446634a7d31da0621c9d484
Bkorob <baka311@yandex.ru>
2024-04-08 21:56:10 +0300

    step3 fix bugs

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('login/', views.UserLoginFormView.as_view(), name='login'),
     path('logout/', views.UserLogoutFormView.as_view(), name='logout'),
     path('users/', include('task_manager.users.urls')),
