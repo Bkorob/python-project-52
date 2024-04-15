9b6498741aac3d21a3c1f2814d17f99d52aa6b26
Bkorob <baka311@yandex.ru>
2024-04-15 05:56:46 +0300

    step8 fix linter bugs

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -158,10 +158,10 @@ STATIC_URL = '/static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
 
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+# and renames the files with unique names for each version to support long-term caching
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
